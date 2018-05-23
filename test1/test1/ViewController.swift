2dab90349455279ac913b710c85c28406fa394f3
qwinner <qwinner@qwinnerdeMacBook-Pro.local>
2018-05-23 22:12:13 +0800

    test1 pro

---
+++ test1/test1/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  test1
+//
+//  Created by qwinner on 2018/5/23.
+//  Copyright © 2018年 qwinner. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+
+class ViewController: UIViewController {
+
+    let label = UILabel()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label.text = "I am the king of the world"
+        label.textAlignment = NSTextAlignment.center
+        self.view.addSubview(label)
+        label.snp.makeConstraints { (make) in
+            make.top.equalTo(100)
+            make.centerX.equalTo(self.view)
+        }
+        label.font = UIFont.systemFont(ofSize: 30)
+        
+        let changeBtn = UIButton(type: .custom)
+        changeBtn.setTitle("Change Font Family", for: UIControlState.normal)
+        changeBtn.addTarget(self, action: #selector(changeFontFamily), for: UIControlEvents.touchUpInside)
+        changeBtn.setTitleColor(UIColor.blue, for: UIControlState.normal)
+        self.view.addSubview(changeBtn)
+        changeBtn.layer.borderColor = UIColor.blue.cgColor
+        changeBtn.layer.borderWidth = 1
+        changeBtn.layer.cornerRadius = 5
+        changeBtn.snp.makeConstraints { (make) in
+            make.top.equalTo(500)
+            make.centerX.equalTo(self.view)
+            make.width.equalTo(200)
+        }
+        printAllSupportedFontNames()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @objc func changeFontFamily() {
+        label.font = UIFont(name: "Savoye LET", size: 30)
+    }
+    
+    func printAllSupportedFontNames() {
+        let familyNames = UIFont.familyNames
+        for familyName in familyNames {
+            print("++++++ \(familyName)")
+            let fontNames = UIFont.fontNames(forFamilyName: familyName)
+            for fontName in fontNames {
+                print("----- \(fontName)")
+            }
+        }
+    }
+
+}
+
